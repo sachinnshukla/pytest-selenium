b82fc02fe90392a8d5ed76c4ee84f5590990637a
Sachin Shukla <Sachin987321shukla@gmail.com>
2025-08-17 04:05:34 +0530

    screensot

--- pages/saucelabs_home_page.py
+++ pages/saucelabs_home_page.py
@@ -4,7 +4,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from .base_page import BasePage
 
 class SauceLabHomePage(BasePage):
-    HOMEPAGE_IDENTIFIER = (By.XPATH, "//span[@class='title' and contains(text(),'Products')]")
+    HOMEPAGE_IDENTIFIER = (By.XPATH, "//span[@class='title' and contains(text(),'Product')]")
 
     def is_home_page_displayed(self):
         home_page_elem = self.wait.until(EC.visibility_of_element_located(self.HOMEPAGE_IDENTIFIER))
